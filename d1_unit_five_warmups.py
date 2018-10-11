e2f4317c6cabe6615c8df9bfe6844c78b53f58f1
Thenea <thenea@iMac4.local>
2018-10-11 08:18:57 -0400

    options with for loops

---
+++ d1_unit_five_warmups.py
@@ -0,0 +1,19 @@
+for x in range(5):
+    # 01234
+    print(x, end="")
+
+
+print()
+
+
+for number in range(2, 8):
+    # 2,3,4,5,6,7
+    print(number, end=",")
+
+
+print()
+
+for zebra in range(10, 20, 2):
+    # 10 13 14 16 18
+    print(zebra, end=" ")
+
