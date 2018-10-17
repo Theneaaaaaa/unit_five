2c56371ca737e88af518e106664ab4ca487c8360
Thenea <thenea@iMac4.local>
2018-10-17 13:46:37 -0400

    The last version of this hard program. I did not make is interesting, but i think I made it work! I will put more efforts in next time and make it fun

--- assignment_five.py
+++ assignment_five.py
@@ -7,11 +7,8 @@
 import random
 
 
-time_to_simulate = int(input("How many times would you like to simulate?"))
-
-
 def roll_the_dice():
-    number = random(1, 6)
+    number = random.randint(1, 6)
     return number
 # get a random number in range 1 to 6, in order to simulate the dice
 
@@ -21,34 +18,36 @@ def re_roll():
     return number
 
 
-def roll_point(point):
-    dice1 = roll_the_dice()
-    dice2 = roll_the_dice()
-    dice_total = dice1 + dice2
-    while dice_total != 7 or dice_total != point:
+def main():
+    time_to_simulate = int(input("How many times would you like to simulate?"))
+    win_time = 0
+    for x in range(time_to_simulate):
         dice1 = roll_the_dice()
         dice2 = roll_the_dice()
         dice_total = dice1 + dice2
-    if dice_total == point:
-        return 1
-    elif dice_total == 7:
-        return 0
+        if dice_total == 7 or dice_total == 11:
+            win_time = win_time + 1
+        elif dice_total == 2 or dice_total == 3 or dice_total == 12:
+            pass
+        # pass it pass it!
+        else:
+            dice3 = roll_the_dice()
+            dice4 = roll_the_dice()
+            dice_total1 = dice3 + dice4
+            while dice_total1 != 7 and dice_total1 != dice_total:
+                # The while loop is needed here for the situation when dice total does not equals to the number
+                dice3 = roll_the_dice()
+                dice4 = roll_the_dice()
+                dice_total1 = dice3 + dice4
+            if dice_total1 == dice_total:
+                win_time = win_time + 1
+    win_rate = win_time / time_to_simulate * 100
+    print("You played", time_to_simulate, "times, you won", win_time, "times and lost", time_to_simulate - win_time, ".")
+    print("you won", win_rate, "percent of the time.")
+    print("Good bye!")
 
 
-def judgement():
-    dice1 = roll_the_dice()
-    dice2 = roll_the_dice()
-    dice_total = dice1 + dice2
-    win_time = 0
-    if dice_total == 7 or 11:
-        win_time = win_time + 1
-    elif dice_total == 2 or 3 or 12:
-        win_time = win_time + 0
-    else:
-        win_time = win_time + roll_point(dice_total)
-
-
-
+main()
 
 
 
